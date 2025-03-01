ef44077d39053eea74f44efc23a0380537a1a6a6
Atila Machan <atiladev@abv.bg>
2025-03-01 17:56:30 +0200

    router controllers

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -6,6 +6,7 @@ const auth = require('./routes/auth');
 app.use(express.static('./methods-public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 app.use('/api/people', people);
 app.use('/login', auth);
 
